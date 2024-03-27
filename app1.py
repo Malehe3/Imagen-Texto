305f9f446119958c029666e710d2cbbb1ea7378f
Malehe3 <160879306+Malehe3@users.noreply.github.com>
2024-03-27 12:06:13 -0500

    Update app1.py

--- app1.py
+++ app1.py
@@ -1,41 +1,30 @@
 import streamlit as st
-import os
+import cv2
+import numpy as np
+import pytesseract
+from PIL import Image
 
-# Ensure the 'temp' directory exists
-if not os.path.exists("temp"):
-    os.makedirs("temp")
 
-def text_to_speech(text, tld):
-    tts = gTTS(text, "es", tld, slow=False)
-    try:
-        my_file_name = text[0:20]
-    except:
-        my_file_name = "audio"
-    save_path = os.path.join("temp", f"{my_file_name}.mp3")  # Construct the file path
-    tts.save(save_path)
-    return my_file_name, text
+st.title("CocinaFacil - Tu Asistente de Cocina Personalizado")
+st.write(f"¡Hola Soy ChefIA, tu asistente de cocina personal. Con solo una foto de una receta, puedo convertirla en texto para que puedas escuchar las instrucciones mientras cocinas y así evitar cualquier accidente")
+st.write(f"Toma una foto")
+img_file_buffer = st.camera_input("Toma una Foto")
 
-if st.button("Convertir texto a audio"):
-    result, output_text = text_to_speech(text, "es")
-    audio_path = os.path.join("temp", f"{result}.mp3")  # Construct the file path
-    if os.path.exists(audio_path):  # Check if the file exists before attempting to read it
-        audio_file = open(audio_path, "rb")
-        audio_bytes = audio_file.read()
-        st.audio(audio_bytes, format="audio/mp3", start_time=0)
-        st.markdown("## Texto en audio:")
-        st.write(output_text)
+with st.sidebar:
+      filtro = st.radio("Aplicar Filtro",('Con Filtro', 'Sin Filtro'))
+
+
+if img_file_buffer is not None:
+    # To read image file buffer with OpenCV:
+    bytes_data = img_file_buffer.getvalue()
+    cv2_img = cv2.imdecode(np.frombuffer(bytes_data, np.uint8), cv2.IMREAD_COLOR)
+    
+    if filtro == 'Con Filtro':
+         cv2_img=cv2.bitwise_not(cv2_img)
     else:
-        st.write("¡Ups! Parece que hubo un error al generar el archivo de audio.")
-
-# Remove files older than 7 days
-def remove_files(n):
-    mp3_files = glob.glob("temp/*.mp3")
-    if len(mp3_files) != 0:
-        now = time.time()
-        n_days = n * 86400
-        for f in mp3_files:
-            if os.path.isfile(f) and os.stat(f).st_mtime < now - n_days:
-                os.remove(f)
-
-remove_files(7)
-
+         cv2_img= cv2_img
+    
+        
+    img_rgb = cv2.cvtColor(cv2_img, cv2.COLOR_BGR2RGB)
+    text=pytesseract.image_to_string(img_rgb)
+    st.write(text) 
